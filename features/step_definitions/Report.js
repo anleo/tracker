36e86451e76b844343a148952a64a6c333fa9f29
alexkorsun <alexgkorsun@gmail.com>
2017-03-07 17:42:26 +0200

    - fixed issue with task developer on server-side when create new task - fixed Report tests

--- features/step_definitions/Report.js
+++ features/step_definitions/Report.js
@@ -5,30 +5,28 @@ module.exports = function () {
     });
 
     this.When(/^I see "([^"]*)" in Done tasks$/, function (arg1, callback) {
-        this.iSee('div li[ng-repeat*="accepted"]:contains("'+arg1+'")', callback)
+        this.iSee('div.accepted li:contains("'+arg1+'")', callback)
     });
 
     this.When(/^I see "([^"]*)" in In progress tasks$/, function (arg1, callback) {
-        this.iSee('div li[ng-repeat*="in progress"]:contains("'+arg1+'")', callback)
+        this.iSee('div.in_progress li:contains("'+arg1+'")', callback)
     });
 
     this.When(/^I see "([^"]*)" in Plans tasks$/, function (arg1, callback) {
-        this.iSee('div li[ng-repeat*="\'\'"]:contains("'+arg1+'")', callback);
+        this.iSee('div.new li:contains("'+arg1+'")', callback);
     });
-
 
     this.Then(/^I share this task on user "([^"]*)"$/, function (arg1, callback) {
         this.chain
-            .iClick("div[ng-model='task.team'] input")
-            .iClick('form div span:contains("' +arg1+ '")')
+            .iClick("task-team-select ng-select input")
+            .iClick('task-team-select ng-select ul.ui-select-choices li div:contains("' +arg1+ '")')
             .then(callback);
     });
 
-
     this.Then(/^I assign it to user "([^"]*)"$/, function (arg1, callback) {
         this.chain
-            .iClick("form select[ng-model='task.developer']")
-            .iClick('form select[ng-model="task.developer"] option:contains("'+arg1+'")')
+            .iClick("task-assign-developer ng-select div.ui-select-container")
+            .iClick('task-assign-developer ng-select ul.ui-select-choices li div:contains("' +arg1+ '")')
             .then(callback);
     });
 
@@ -37,26 +35,26 @@ module.exports = function () {
     });
 
     this.Then(/^I see "([^"]*)" in assigned users$/, function (arg1, callback) {
-        this.iSee('select[ng-model="developer"]:contains("'+arg1+'")', callback);
+        this.iSee('div.report-page ng-select div.ui-select-container span:contains("'+arg1+'")', callback);
     });
 
     this.When(/^I choose user "([^"]*)"$/, function (arg1, callback) {
         this.chain
-            .iClick('select[ng-model="developer"]')
-            .iClick('select[ng-model="developer"] option:contains("'+arg1+'")')
+            .iClick("ng-select div.ui-select-container")
+            .iClick('ng-select div.ui-select-container div:contains("' +arg1+ '")')
             .then(callback);
     });
 
     this.Then(/^I don't see "([^"]*)" in Plans tasks$/, function (arg1, callback) {
-        this.iDontSee('div li[ng-repeat*="\'\'"]:contains("'+arg1+'")', callback);
+        this.iDontSee('div.new li:contains("'+arg1+'")', callback);
     });
 
     this.Then(/^I don't see "([^"]*)" in Done tasks$/, function (arg1, callback) {
-        this.iDontSee('div li[ng-repeat*="accepted"]:contains("'+arg1+'")', callback);
+        this.iDontSee('div.accepted li:contains("'+arg1+'")', callback);
     });
 
     this.When(/^I don't see "([^"]*)" in In progress tasks$/, function (arg1, callback) {
-        this.iDontSee('div li[ng-repeat*="in progress"]:contains("'+arg1+'")', callback);
+        this.iDontSee('div.in_progress li:contains("'+arg1+'")', callback);
     });
 
     this.Then(/^I click back to project "([^"]*)"$/, function (arg1, callback) {
