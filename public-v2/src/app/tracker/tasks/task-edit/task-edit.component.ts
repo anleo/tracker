6f7eda9c3058a0b24a7c556350f6d5201e566948
anleo <skaliozz@gmail.com>
2017-03-14 17:44:11 +0200

    - cleanup

--- public-v2/src/app/tracker/tasks/task-edit/task-edit.component.ts
+++ public-v2/src/app/tracker/tasks/task-edit/task-edit.component.ts
@@ -32,7 +32,6 @@ export class TasksEditComponent implements OnInit {
               private taskStatusService: TaskStatusService,
               public vcr: ViewContainerRef,
               private route: ActivatedRoute,
-              private localStorageService:LocalStorageService,
               public toastr: ToastsManager) {
     this.toastr.setRootViewContainerRef(vcr);
   }
@@ -97,8 +96,6 @@ export class TasksEditComponent implements OnInit {
     this.closeModal();
     this.taskService.editTaskUpdated$.next({task: task, status: 'update'});
     setTimeout(() => this.initTask(), 0);
-
-    this.localStorageService.set('lastEditedTask', task);
   }
 
   getInitEditedTask(task: Task) {
