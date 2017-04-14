3c4a598e29e8a8a5109634da8166d148e46d9125
Dmitry Ryazantsev <dsr.workk@gmail.com>
2017-04-14 18:14:30 +0300

    - updated BoardItemService method update and findById - renamed boardItem param to boardItemId

--- app/services/board/BoardItemService.js
+++ app/services/board/BoardItemService.js
@@ -11,7 +11,6 @@ let BoardItemService = function (Board,
             BoardItem
                 .findById(id)
                 .populate('item')
-                .lean()
                 .exec()
                 .then((item) => resolve(item), (err) => reject(err));
         })
@@ -50,18 +49,14 @@ let BoardItemService = function (Board,
         });
     };
 
-    //TODO @@@dr converse update with @feya because create different
     this.update = function (boardItem, data) {
         return new Promise(function (resolve, reject) {
-            BoardItem.findById(boardItem._id)
-                .then((boardItem) => {
-                        _.assign(boardItem, data);
+                _.assign(boardItem, data);
 
-                        boardItem.save()
-                            .then((boardItem) => resolve(boardItem), (err) => reject(err))
-                    },
-                    (err) => reject(err));
-        });
+                boardItem.save()
+                    .then((boardItem) => resolve(boardItem), (err) => reject(err))
+            },
+            (err) => reject(err));
     }
 
     this.createBoardItem = function (data) {
@@ -125,7 +120,7 @@ let BoardItemService = function (Board,
         return new Promise(function (resolve, reject) {
             BoardItem.findById(id)
                 .then((boardItem) => {
-                //TODO @@@ira if !boardItem check
+                        //TODO @@@ira if !boardItem check
                         boardItem.remove()
                             .then(() => {
                                     resolve();
