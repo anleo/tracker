ac22041dc16b13ccdff05fc8bca1312cf4c02362
alexkorsun <alexgkorsun@gmail.com>
2017-04-21 15:27:42 +0300

    - fixed check for parent in task update API endpoint

--- app/api/task/index.js
+++ app/api/task/index.js
@@ -254,11 +254,11 @@ module.exports = function (app) {
     app.put('/api/tasks/:taskId', TaskForm, FormService.validate, function (req, res, next) {
         var taskData = _.merge({parentTaskId: req.body.parentTaskId || null}, req.form);
 
-        if (taskData.parentTaskId.toString() === req.Task._id.toString()) {
-            return res.status(403).json({error: 'You can\'t add task into itself'});
-        }
-
         if (taskData.parentTaskId) {
+            if (taskData.parentTaskId.toString() === req.Task._id.toString()) {
+                return res.status(403).json({error: 'You can\'t add task into itself'});
+            }
+
             /// @@@ re-think and refactor
             TaskService.getTaskById(taskData.parentTaskId, function (err, parent) {
                 TaskService.hasAccess(parent, req.user, function (err, access) {
