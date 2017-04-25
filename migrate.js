258cb0237a86e4e413332d74a4f5aa95caf9685c
alexkorsun <alexgkorsun@gmail.com>
2017-04-25 17:04:21 +0300

    - updated migrate.js

--- migrate.js
+++ migrate.js
@@ -2,7 +2,12 @@ let migrate = require('migrate');
 let set = migrate.load('migrations/.migrate', 'migrations');
 
 set.up(function (err) {
-    if (err) throw err;
+    if (err) {
+        console.error(err);
+        err.stack && console.error(err.stack);
+        return process.exit(1);
+    }
 
     console.log('Migration completed');
+    process.exit(0);
 });
