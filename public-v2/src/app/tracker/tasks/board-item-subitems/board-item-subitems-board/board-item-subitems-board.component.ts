386065f05e6c46ab4611084b6c5a11d0a5c69418
Irina Drozdova <irina.drozdova.work@gmail.com>
2017-05-04 19:52:01 +0300

    - fixed board-item-subitems-board-component for loading children on add new child

--- public-v2/src/app/tracker/tasks/board-item-subitems/board-item-subitems-board/board-item-subitems-board.component.ts
+++ public-v2/src/app/tracker/tasks/board-item-subitems/board-item-subitems-board/board-item-subitems-board.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input} from "@angular/core";
+import {Component, OnInit, Input, OnChanges} from "@angular/core";
 import {BoardItemService} from "../../../services/board-item.service";
 import {TaskBoardItem} from "../../../models/task-board-item";
 
@@ -7,16 +7,28 @@ import {TaskBoardItem} from "../../../models/task-board-item";
   templateUrl: 'board-item-subitems-board.component.html'
 })
 
-export class BoardItemSubitemsBoardComponent implements OnInit{
+export class BoardItemSubitemsBoardComponent implements OnInit, OnChanges {
   @Input() parentBoardItem: TaskBoardItem | null;
   @Input() refreshCount;
   @Input() sortOptions;
-
+  oldRefreshCount: number;
   boardItems: TaskBoardItem[] = [];
 
-  constructor(private boardItemService: BoardItemService) {}
+  constructor(private boardItemService: BoardItemService) {
+  }
 
   ngOnInit(): void {
+    this.loadChildren();
+  }
+
+  ngOnChanges(): void {
+    if (this.oldRefreshCount !== this.refreshCount) {
+      this.loadChildren();
+      this.oldRefreshCount = this.refreshCount;
+    }
+  }
+
+  private loadChildren() {
     let boardId = this.parentBoardItem && this.parentBoardItem.item && this.parentBoardItem.item._id ?
       this.parentBoardItem.item._id : this.parentBoardItem.item;
     this.boardItemService
